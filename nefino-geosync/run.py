6b6c5a06709ec545a99d2c08d4af0a3b661b89d9
Chris Stetter <chris.stetter@nefino.de>
2024-11-08 11:13:00 +0000

    Add main function to entry point

--- nefino-geosync/run.py
+++ nefino-geosync/run.py
@@ -6,19 +6,23 @@ from .download_completed_analyses import download_completed_analyses
 from .config import Config
 from .parse_args import parse_args
 
-args = parse_args()
+def main():
+    args = parse_args()
 
-if args.configure:
-    config = Config.singleton()
-    # if you are running with --configure on the first run (you don't need to)
-    # you will be prompted to configure the app by the config singleton init.
-    # In that case, don't prompt the user again.
-    if not config.already_prompted:
-        config.run_config_prompts()
+    if args.configure:
+        config = Config.singleton()
+        # if you are running with --configure on the first run (you don't need to)
+        # you will be prompted to configure the app by the config singleton init.
+        # In that case, don't prompt the user again.
+        if not config.already_prompted:
+            config.run_config_prompts()
 
-client = get_client(api_host=os.getenv("NEFINO_API_HOST", default="https://api.nefino.li"))
+    client = get_client(api_host=os.getenv("NEFINO_API_HOST", default="https://api.nefino.li"))
 
-if not args.resume:
-    start_analyses(client)
-else:
-    download_completed_analyses(client)
+    if not args.resume:
+        start_analyses(client)
+    else:
+        download_completed_analyses(client)
+
+if __name__ == "__main__":
+    main()
